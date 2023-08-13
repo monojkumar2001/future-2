59cec232c369cf6cfdbf0ccfab809bc8c488d239
monoj <monojkumar57859893@gmail.com>
2023-08-13 11:19:09 +0600

    all done

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+
+        <>
+           {/* =========== Footer start ======================= */}
+            {/* <section className='footer'>
+                <div className="footer-bg">
+                    <img src="./images/footer-bg.png" alt="" />
+                </div>
+                <div className='footer-con'>
+                    <div className='socials'>
+                        <a href='' target='_blank'>
+                            <img src="./images/discord.png" alt="discord" />
+                        </a>
+                        <a href='' target='_blank'>
+                            <img src="./images/twitter.png" alt="twitter" />
+                        </a>
+                        <a href='' target='_blank'>
+                            <img src="./images/opensea.png" alt="instagram" />
+                        </a>
+                    </div>
+                    <div className='copyright'>
+                        <p>© 2022 - ALTLANTEANS | All Rights </p>
+                    </div>
+                    <div className='nft-constructer'>
+                        <p>
+                            <a href='https://www.nftconstructer.com/' target='_blank'><img src="./images/nft-constructor.png" alt="NFT CONSTRUCTER" />NFT CONSTRUCTER</a>
+                        </p>
+                    </div>
+                </div>               
+            </section> */}
+           {/* =========== Footer  ======================= */}
+        </>
+
+    )
+
+}
+
+export default Footer;
+
+
+
